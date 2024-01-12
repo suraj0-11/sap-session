bca78e808ceecdf0466ce5c3d2a62a60b8f1ff38
Suraj <116282102+suraj0-11@users.noreply.github.com>
2024-01-12 22:04:06 +0530

    Create day1.sh

---
+++ day1.sh
@@ -0,0 +1,15 @@
+uraj@suraj-1-0:~$ cd 'sap session'
+suraj@suraj-1-0:~/sap session$ mkdir edunet
+suraj@suraj-1-0:~/sap session$ cd edunet
+suraj@suraj-1-0:~/sap session/edunet$ echo "hello this is suraj" >> file1.txt
+suraj@suraj-1-0:~/sap session/edunet$ cat file1.txt
+hello this is suraj
+suraj@suraj-1-0:~/sap session/edunet$ echo "ohh fine it works"
+ohh fine it works
+suraj@suraj-1-0:~/sap session/edunet$ cp file1.txt file2.txt
+suraj@suraj-1-0:~/sap session/edunet$ cat file2.txt
+hello this is suraj
+suraj@suraj-1-0:~/sap session/edunet$ rm file1.txt
+suraj@suraj-1-0:~/sap session/edunet$ ls
+file2.txt
+suraj@suraj-1-0:~/sap session/edunet$ 
